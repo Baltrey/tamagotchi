5c6ea38b1358553f01f9b5dfb60143cb0be7fd95
Baltazar Biverstal <baltazar.biverstal@elev.ga.ntig.se>
2023-10-16 11:14:18 +0200

    gjorde klart spelet

--- Program.cs
+++ Program.cs
@@ -1,5 +1,59 @@
-﻿Tamagotchi tama = new Tamagotchi();
+﻿Tamagotchi Tama = new Tamagotchi();
+string[] choices = { "Feed", "Hi", "Teach", "Ingenting" };
+
+while (true)
+{
+    Console.WriteLine("Vad vill du döpa din tamagotchi?");
+    Tama.name = Console.ReadLine();
+    while (Tama.GetAlive())
+    {
+        Console.WriteLine("vad vill du göra " + string.Join(",", choices));
+        string a = answer();
+        if (a == "FEED" || a == "1")
+        {
+            Tama.Feed();
+            Tama.Tick();
+        }
+        if (a == "HI" || a == "2")
+        {
+            Tama.Hi();
+            Tama.Tick();
+        }
+        if (a == "TEACH" || a == "3")
+        {
+            Tama.Teach();
+            Tama.Tick();
+        }
+        else
+        {
+            Tama.Tick();
+        }
+    }
+    bool goodanswer = true;
+    while (goodanswer)
+    {
+        Console.WriteLine("Vill du köra igen?");
+        string a = answer();
+        if (a == "YES" || a == "Y")
+        {
+            goodanswer = false;
+        }
+        if (a == "NO" || a == "N")
+        {
+            return;
+        }
+        else
+        {
+
+        }
+    }
+
+}
 
 
-tama.Printstat();
-Console.ReadLine();
+string answer()
+{
+    string answers = Console.ReadLine();
+    answers = answers.ToUpper();
+    return answers;
+}
--- class.cs
+++ class.cs
@@ -1,28 +1,66 @@
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
 public class Tamagotchi
 {
-    int hunger = 100;
-    int boredom = 100;
-    List<string> words = new();
+    int hunger = 80;
+    int boredom = 80;
+    List<string> words = new() { "Hej", "blomma", "fjäril", "plan", "bil", "sol", "måne", "penna", "sudd" };
+    int x;
     Random generator = new();
     public string name;
 
     public void Feed()
     {
-
+        if (hunger < 100)
+        {
+            hunger += 15;
+            Console.WriteLine("Du gav " + name + " mat!");
+            Printstat();
+        }
+        else
+        {
+            Console.WriteLine(name + " är inte hungrig:(");
+        }
     }
 
     public void Hi()
     {
-
+        if (boredom < 100)
+        {
+            ReduceBoredom();
+            Console.WriteLine("Du sa hej till " + name + "!");
+            Printstat();
+        }
+        else
+        {
+            Console.WriteLine(name + " blev glad men är inte utråkad");
+        }
     }
     public void Teach()
     {
-
+        if (boredom < 100)
+        {
+            if (x <= 9)
+            {
+                Console.WriteLine("Du lärde " + name + " ordet: " + words[x]);
+                ReduceBoredom();
+                x++;
+            }
+            else
+            {
+                Console.WriteLine("Du lärde inga ny ord men " + name + " blev glad för försöket");
+                ReduceBoredom();
+            }
+        }
+        else
+        {
+            Console.WriteLine(name + " blev glad men är inte utråkad");
+        }
     }
     public void Tick()
     {
-
+        boredom -= 10;
+        hunger -= 10;
     }
     public void Printstat()
     {
@@ -31,10 +69,17 @@ public class Tamagotchi
     }
     public bool GetAlive()
     {
-        return true;
+        if (boredom > 0 && hunger > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     void ReduceBoredom()
     {
-
+        boredom += 15;
     }
 }
